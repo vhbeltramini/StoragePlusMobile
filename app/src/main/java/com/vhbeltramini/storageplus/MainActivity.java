f59f318fbf3dafb2f0a48bbe2825b277719a4576
Victor Hugo Grabowski Beltramini <vhbeltramini@gmail.com>
2021-11-23 20:11:39 -0300

    Initial StoragePlus mobile project.

---
+++ app/src/main/java/com/vhbeltramini/storageplus/MainActivity.java
@@ -0,0 +1,23 @@
+package com.vhbeltramini.storageplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    SignInButton signInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+    }
+}
