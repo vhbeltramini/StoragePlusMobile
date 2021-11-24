1619c5cd55fa421dda46fb57f3a948b41d98f913
Bruce Vahldick <brucev.br@gmail.com>
2021-11-24 19:13:19 -0300

    Update (adicionado o AtributoDinamicoDatabase.java)

---
+++ app/src/main/java/com/vhbeltramini/storageplus/Database/AtributoDinamicoDatabase.java
@@ -0,0 +1,29 @@
+package com.vhbeltramini.storageplus.Database;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.vhbeltramini.storageplus.Dao.AtributoDinamicoDao;
+import com.vhbeltramini.storageplus.Model.AtributoDinamico;
+
+@Database(entities = {AtributoDinamico.class}, exportSchema = false, version = 1)
+public abstract class AtributoDinamicoDatabase extends RoomDatabase{
+
+    private static final String DB_NAME = "dbstorageplus";
+    private static AtributoDinamicoDatabase instance;
+
+    public static synchronized AtributoDinamicoDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), AtributoDinamicoDatabase.class, DB_NAME)
+                    .allowMainThreadQueries().fallbackToDestructiveMigration().build();
+        }
+        return instance;
+    }
+
+    public abstract AtributoDinamicoDao atributoDinamicoDao();
+
+}
